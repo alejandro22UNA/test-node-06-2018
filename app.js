9c5118127042c4833642c7e65bcd1c6114b1e76d
alejandro22UNA <e.morales.ale22@gmail.com>
2018-06-01 01:26:06 -0600

    03

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ app.set('view engine', 'ejs');
 var app = express();
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json());
+
+app.set('port', (process.env.PORT || 5000))
 app.use(express.static('public'));
 
 
@@ -25,6 +27,6 @@ var routes = require('./routes');
 connection.inicia();
 routes.configurar(app);
 
-var server = app.listen(8000, function() {
+var server = app.listen('port', function() {
    console.log('Escuchando en el puerto ', server.address().port);
 })
